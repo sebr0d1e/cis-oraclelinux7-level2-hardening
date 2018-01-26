5cdb93461337296793f9f03c65be745a8b002a31
sebr0d1e <steve.brodie@us.fujitsu.com>
2018-01-26 13:23:32 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# cis-oraclelinux7-level2-hardening
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: cis-oraclelinux7-level2-hardening
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
